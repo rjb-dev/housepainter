9255b03f3b40798f9da2fb125d28c6f7ef18eae0
Rebecca Blundell <rebecca.blundell@catalyst.net.nz>
2018-07-27 15:01:08 +1200

    first commit

---
+++ public/config.php
@@ -0,0 +1,10 @@
+<?php
+$config = array();
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'painteruser';
+$config['db']['pass']   = 'paint';
+$config['db']['dbname'] = 'housepainter';
